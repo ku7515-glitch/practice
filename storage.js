df8e875e81e67615958157ad689c5c1d82a24a16
KU SEOHYUN <ku7515@gmail.com>
2026-07-29 22:10:34 +0900

    chore: main 브랜치 초기화 (테스트 파일 삭제)

--- storage.js
+++
@@ -1,8 +0,0 @@
-export const Storage = {
-    save: (key, data) => localStorage.setItem(key, JSON.stringify(data)),
-    get: (key) => {
-        const data = localStorage.getItem(key);
-        return data ? JSON.parse(data) : null;
-    },
-    remove: (key) => localStorage.removeItem(key)
-};
